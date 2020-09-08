a2825d1773f9b8ab3291fcf238b3fb3efcb93fd7
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-09-08 18:15:25 +0200

    update

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = {
-        plugins: [
-            require('autoprefixer'),
-            require('cssnano'),
-            // More postCSS modules here if needed
-        ]
-    }
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,61 +1,64 @@
-import '../sass/styles.scss';
+// Navbar element
 
-// Animated Text
+const sections = [...document.querySelectorAll('section')];
+const link = (id) => document.querySelector(`a[href="#${id}"]`)
 
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
+const inView = (element) => {
+    var top = element.offsetTop;
+    var height = element.offsetHeight;
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
+    while(element.offsetParent){
+        element = element.offsetParent;
+        top += element.offsetTop;
+    }
+
+    return (
+        top < (window.pageYOffset + window.innerHeight) && (top + height) > window.pageYOffset
+        );
+    };
+
+const init = () => {
+    function update(){
+        let next = false;
+
+        sections.forEach(section => {
+            const current = link(section.id);
+
+            if(inView(section) && !next ){
+                current.classList.add('current');
+                next = true;
+            } else {
+                current.classList.remove('current');
+            }
+        } );
+    }
+    update();
+    window.addEventListener('scroll', update);
 }
 
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-  
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-  
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
-
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
-
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-  
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
+init();
 
 
-// END Animated Text
+// scroll to top button
+const scrollToTop = document.getElementById('scroll-to-top');
+let dataShow = false;
+
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY > 50 && !dataShow){
+        scrollToTop.setAttribute('data-show', 'true');
+        dataShow = true;
+    }
+
+    if(window.scrollY <= 50 && dataShow){
+        scrollToTop.setAttribute('data-show', 'false');
+        dataShow = false;
+    }
+});
+
+scrollToTop.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
