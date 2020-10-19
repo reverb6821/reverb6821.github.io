2443c118fa69e70555e18effdc550e9cefca1556
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-10-19 11:16:04 +0200

    portfolio js completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,90 +1,155 @@
 //Portfolio Filter
-
-const filterContainer = document.querySelector('.portfolio-filter');
+const filterContainer = document.querySelector(".portfolio-filter");
 const filterBtns = filterContainer.children;
-const totaFilterBtn = filterBtns.length;
-const portfolioItems = document.querySelector('.portfolio-item');
+const portfolioItems = document.querySelectorAll(".portfolio-item");
 const totalPortfolioItem = portfolioItems.length;
 
-for(let i = 0; i < totaFilterBtn; i++){
+for (let i = 0; i < filterBtns.length; i++) {
   //console.log(this.innerHTML)
-  filterBtns[i].addEventListener('click', function(){
-    filterContainer.querySelector('.active').classList.remove('active');
-    this.classList.add('active');
-    
-    const filterValue = this.getAttribute('data-filter');
-    for(let k = 0; k < totalPortfolioItem; k++){
-      if(filterValue === portfolioItems[k].getAttribute('data-category')){
-        portfolioItems[k].classList.remove('hide');
-        portfolioItems[k].classList.add('show');
-      }else if(filterValue === 'all'){
-        portfolioItems[k].classList.remove('hide');
-        portfolioItems[k].classList.add('show');
-      }else{
-        portfolioItems[k].classList.remove('show');
-        portfolioItems[k].classList.add('hide');
+  filterBtns[i].addEventListener("click", function () {
+    filterContainer.querySelector(".active").classList.remove("active");
+    this.classList.add("active");
+
+    const filterValue = this.getAttribute("data-filter");
+
+    for (let k = 0; k < portfolioItems.length; k++) {
+      if (filterValue === portfolioItems[k].getAttribute("data-category")) {
+        portfolioItems[k].classList.remove("hide");
+        portfolioItems[k].classList.add("show");
+      } else if (filterValue === "all") {
+        portfolioItems[k].classList.remove("hide");
+        portfolioItems[k].classList.add("show");
+      } else {
+        portfolioItems[k].classList.remove("show");
+        portfolioItems[k].classList.add("hide");
       }
     }
-  })
+  });
 }
 
+//Portfolio detail
+const details = document.querySelector(".lightbox");
+const imgDetails = details.querySelector(".lightbox-img");
+const closeDetails = details.querySelector("#portfolio-close");
+const textDetails = details.querySelector(".caption-text");
+const counterDetails = details.querySelector(".caption-counter");
 
+let itemIndex = 0;
+
+for (let i = 0; i < totalPortfolioItem; i++) {
+  portfolioItems[i].addEventListener("click", function () {
+    itemIndex = i;
+    changeItem();
+    toggleDetails();
+  });
+}
+
+nextItem = () => {
+  if (itemIndex == totalPortfolioItem - 1) {
+    itemIndex = 0;
+  } else {
+    itemIndex++;
+  }
+  changeItem();
+};
+
+prevItem = () => {
+  if (itemIndex == 0) {
+    itemIndex = totalPortfolioItem - 1;
+  } else {
+    itemIndex--;
+  }
+  changeItem();
+};
+
+toggleDetails = () => {
+  details.classList.toggle("open");
+};
+
+changeItem = () => {
+  imgSrc = portfolioItems[itemIndex]
+    .querySelector(".portfolio-img img")
+    .getAttribute("src");
+  imgDetails.src = imgSrc;
+  textDetails.innerHTML = portfolioItems[itemIndex].querySelector(
+    "h4"
+  ).innerHTML;
+  counterDetails.innerHTML = itemIndex + 1 + " of " + totalPortfolioItem;
+};
+
+details.addEventListener("click", function(event){
+  if(event.target === closeDetails){
+    toggleDetails();
+  }
+})
 
 // text rotation
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+// class TxtRotate2 {
+//   constructor(el, toRotate, period){
+
+//   }
+
+//   static tick() {
+
+//   }
+// }
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("txt-rotate");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-rotate");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
-  
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-  };
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
 // END Animated txt
