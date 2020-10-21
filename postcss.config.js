7a121fd678b09526aa50852122ffe9a7bc3c5651
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-10-21 16:22:27 +0200

    Completed

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+
+if(process.env.NODE_ENV === 'production') {
+    module.exports = {
+        plugins: [
+            require('autoprefixer'),
+            require('cssnano')
+        ]
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,89 +1,38 @@
-//Portfolio Filter
-const filterContainer = document.querySelector(".portfolio-filter");
-const filterBtns = filterContainer.children;
-const portfolioItems = document.querySelectorAll(".portfolio-item");
-const totalPortfolioItem = portfolioItems.length;
+import '../scss/styles.scss';
 
-for (let i = 0; i < filterBtns.length; i++) {
-  //console.log(this.innerHTML)
-  filterBtns[i].addEventListener("click", function () {
-    filterContainer.querySelector(".active").classList.remove("active");
-    this.classList.add("active");
+// !Menu
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
+const menuPortrait = document.querySelector(".menu-portrait");
+const navItems = document.querySelectorAll(".nav-item");
 
-    const filterValue = this.getAttribute("data-filter");
+let showMenu = false; // *Hide Menu on start
 
-    for (let k = 0; k < portfolioItems.length; k++) {
-      if (filterValue === portfolioItems[k].getAttribute("data-category")) {
-        portfolioItems[k].classList.remove("hide");
-        portfolioItems[k].classList.add("show");
-      } else if (filterValue === "all") {
-        portfolioItems[k].classList.remove("hide");
-        portfolioItems[k].classList.add("show");
-      } else {
-        portfolioItems[k].classList.remove("show");
-        portfolioItems[k].classList.add("hide");
-      }
-    }
-  });
+menuBtn.addEventListener("click", toggleMenu); // *Activate hamb menu
+
+// *Hide/Show Menu Function
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuPortrait.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    showMenu = true; // !Show Menu
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuPortrait.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    showMenu = false; // !Hide Menu
+  }
 }
 
-//Portfolio detail
-const details = document.querySelector(".lightbox");
-const imgDetails = details.querySelector(".lightbox-img");
-const closeDetails = details.querySelector("#portfolio-close");
-const textDetails = details.querySelector(".caption-text");
-const counterDetails = details.querySelector(".caption-counter");
-
-let itemIndex = 0;
-
-for (let i = 0; i < totalPortfolioItem; i++) {
-  portfolioItems[i].addEventListener("click", function () {
-    itemIndex = i;
-    changeItem();
-    toggleDetails();
-  });
-}
-
-nextItem = () => {
-  if (itemIndex == totalPortfolioItem - 1) {
-    itemIndex = 0;
-  } else {
-    itemIndex++;
-  }
-  changeItem();
-};
-
-prevItem = () => {
-  if (itemIndex == 0) {
-    itemIndex = totalPortfolioItem - 1;
-  } else {
-    itemIndex--;
-  }
-  changeItem();
-};
-
-toggleDetails = () => {
-  details.classList.toggle("open");
-};
-
-changeItem = () => {
-  imgSrc = portfolioItems[itemIndex]
-    .querySelector(".portfolio-img img")
-    .getAttribute("src");
-  imgDetails.src = imgSrc;
-  textDetails.innerHTML = portfolioItems[itemIndex].querySelector(
-    "h4"
-  ).innerHTML;
-  counterDetails.innerHTML = itemIndex + 1 + " of " + totalPortfolioItem;
-};
-
-details.addEventListener("click", function(event){
-  if(event.target === closeDetails){
-    toggleDetails();
-  }
-})
-
-// text rotation
+// !Text Rotation
 var TxtRotate = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -127,16 +76,6 @@ TxtRotate.prototype.tick = function () {
   }, delta);
 };
 
-// class TxtRotate2 {
-//   constructor(el, toRotate, period){
-
-//   }
-
-//   static tick() {
-
-//   }
-// }
-
 window.onload = function () {
   var elements = document.getElementsByClassName("txt-rotate");
   for (var i = 0; i < elements.length; i++) {
@@ -146,10 +85,11 @@ window.onload = function () {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-  // INJECT CSS
+  // *INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
-// END Animated txt
+
+// ! Portfolio Gallery
