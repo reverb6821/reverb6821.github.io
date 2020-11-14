75d94aa3c745fbad61ae66bf5701be3c8e962382
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-11-14 21:32:02 +0100

    portfolio update

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,10 @@
-import '../scss/styles.scss';
-
-// !Menu
-const menu = document.querySelector(".menu");
-const menuBtn = document.querySelector(".menu-btn");
-const menuNav = document.querySelector(".menu-nav");
-const menuPortrait = document.querySelector(".menu-portrait");
-const navItems = document.querySelectorAll(".nav-item");
-
-let showMenu = false; // *Hide Menu on start
-
-menuBtn.addEventListener("click", toggleMenu); // *Activate hamb menu
-
-// *Hide/Show Menu Function
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuPortrait.classList.add("show");
-    navItems.forEach((item) => item.classList.add("show"));
-
-    showMenu = true; // !Show Menu
-  } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuPortrait.classList.remove("show");
-    navItems.forEach((item) => item.classList.remove("show"));
-
-    showMenu = false; // !Hide Menu
-  }
-}
-
 // !Text Rotation
 var TxtRotate = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
+  this.txt = '';
   this.tick();
   this.isDeleting = false;
 };
@@ -53,7 +19,7 @@ TxtRotate.prototype.tick = function () {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 300 - Math.random() * 100;
@@ -65,7 +31,7 @@ TxtRotate.prototype.tick = function () {
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
@@ -77,19 +43,17 @@ TxtRotate.prototype.tick = function () {
 };
 
 window.onload = function () {
-  var elements = document.getElementsByClassName("txt-rotate");
+  var elements = document.getElementsByClassName('txt-rotate');
   for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-rotate");
-    var period = elements[i].getAttribute("data-period");
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
   // *INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
   document.body.appendChild(css);
 };
-
-// ! Portfolio Gallery
