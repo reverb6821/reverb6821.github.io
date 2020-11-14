431d4690902c47598fcdc51db6024d447c8ce0d8
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-11-14 23:53:33 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -12,11 +12,9 @@ $(function () {
     gsap.to('#navigation-content', 0, { display: 'none', delay: 0.7 });
     gsap.to('#navigation-content', 0, { y: '-100%', delay: 0.7 });
     gsap.to('#header', 0, { display: 'none' });
-    gsap.to('#blog', 0, { display: 'none' });
     gsap.to('#portfolio', 0, { display: 'none' });
     gsap.to('#breaker', 0, { display: 'block' });
     gsap.to('#breaker-two', 0, { display: 'block', delay: 0.1 });
-    gsap.to('#contact', 0, { display: 'none' });
     gsap.to('#breaker', 0, { display: 'none', delay: 2 });
     gsap.to('#breaker-two', 0, { display: 'none', delay: 2 });
     gsap.to('#about', 0, { display: 'block', delay: 0.7 });
@@ -27,8 +25,6 @@ $(function () {
     gsap.to('#navigation-content', 0, { y: '-100%', delay: 0.7 });
     gsap.to('#header', 0, { display: 'none' });
     gsap.to('#about', 0, { display: 'none' });
-    gsap.to('#contact', 0, { display: 'none' });
-    gsap.to('#blog', 0, { display: 'none' });
     gsap.to('#breaker', 0, { display: 'block' });
     gsap.to('#breaker-two', 0, { display: 'block', delay: 0.1 });
     gsap.to('#breaker', 0, { display: 'none', delay: 2 });
@@ -42,8 +38,6 @@ $(function () {
     gsap.to('#header', 0, { display: 'none' });
     gsap.to('#about', 0, { display: 'none' });
     gsap.to('#portfolio', 0, { display: 'none' });
-    gsap.to('#contact', 0, { display: 'none' });
-    gsap.to('#blog', 0, { display: 'none' });
     gsap.to('#breaker', 0, { display: 'block' });
     gsap.to('#breaker-two', 0, { display: 'block', delay: 0.1 });
     gsap.to('#breaker', 0, { display: 'none', delay: 2 });
