eefaa1fac2af1ad9e84081c6386b3da67344a4e7
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-04-20 22:32:48 +0200

    fix

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,69 +11,49 @@ function Home() {
       <Container fluid className='home-section' id='home'>
         <Particles />
         <Container className='home-content'>
-          <Row>
-            <Col md={7} className='home-header'>
-              <h1 style={{ paddingBottom: 15 }} className='heading'>
-                Hello World!
-              </h1>
+          <h1 className='heading-name'>
+            I'M
+            <strong className='main-name'> Giuseppe Gigliotti</strong>
+          </h1>
+          <div style={{ padding: 50, textAlign: 'left' }}>
+            <Type />
+          </div>
 
-              <h1 className='heading-name'>
-                I'M
-                <strong className='main-name'> Giuseppe Gigliotti</strong>
-              </h1>
-
-              <div style={{ padding: 50, textAlign: 'left' }}>
-                <Type />
-              </div>
-
-              <ul className='home-about-social-links'>
-                <li className='social-icons'>
-                  <a
-                    href='https://github.com/soumyajit4419'
-                    target='_blank'
-                    rel='noreferrer'
-                    className='icon-colour  home-social-icons'
-                  >
-                    <i className='fab fa-github'></i>
-                  </a>
-                </li>
-                <li className='social-icons'>
-                  <a
-                    href='https://twitter.com/Soumyajit4419'
-                    target='_blank'
-                    rel='noreferrer'
-                    className='icon-colour  home-social-icons'
-                  >
-                    <i className='fab fa-twitter'></i>
-                  </a>
-                </li>
-                <li className='social-icons'>
-                  <a
-                    href='https://www.linkedin.com/in/soumyajit4419/'
-                    target='_blank'
-                    rel='noreferrer'
-                    className='icon-colour  home-social-icons'
-                  >
-                    <i className='fab fa-linkedin-in'></i>
-                  </a>
-                </li>
-                <li className='social-icons'>
-                  <a
-                    href='https://www.instagram.com/s.o.u.m.y.a_j.i.t/'
-                    target='_blank'
-                    rel='noreferrer'
-                    className='icon-colour home-social-icons'
-                  >
-                    <i className='fab fa-instagram'></i>
-                  </a>
-                </li>
-              </ul>
-            </Col>
-
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img src={homeLogo} alt='home pic' className='img-fluid' />
-            </Col>
-          </Row>
+          <Col>
+            {' '}
+            <ul className='home-about-social-links'>
+              <li className='social-icons'>
+                <a
+                  href='https://github.com/reverb6821'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
+                >
+                  <i className='fab fa-github'></i>
+                </a>
+              </li>
+              <li className='social-icons'>
+                <a
+                  href='https://www.linkedin.com/in/gigliottigiuseppe/'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
+                >
+                  <i className='fab fa-linkedin-in'></i>
+                </a>
+              </li>
+              <li className='social-icons'>
+                <a
+                  href='https://codepen.io/reverbod'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour home-social-icons'
+                >
+                  <i className='fab fa-codepen'></i>
+                </a>
+              </li>
+            </ul>
+          </Col>
         </Container>
       </Container>
     </section>
