33563b0b5684c8894651162353645dc528577f0a
reverb6821 <giuseppegigliottidev@gmail.com>
2022-05-28 10:53:59 +0200

    moved to vite react

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,129 @@
-import React, { useState, useRef, useEffect } from 'react'
-import styled from 'styled-components'
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-import Theme from './assets/style/Theme'
+function App() {
+  const [count, setCount] = useState(0)
 
-import { Navbar } from './Components/partials/navbar/Navbar'
-import { Sidebar } from './Components/partials/sidebar/Sidebar'
-import { Main } from './view/Main'
-
-const AppContainer = styled.div`
-    background: #1e1e1e;
-    font-family: 'Lato', sans-serif; ;
-`
-
-function App(): any {
-    const [scrollProgress, setScrollProgress] = useState('20%')
-    const [didScroll, setDidScroll] = useState(false)
-    const [isSafari, setIsSafari] = useState(false)
-
-    const heroRef = useRef(null)
-    const aboutRef = useRef(null)
-    const skillsRef = useRef(null)
-    const projectRef = useRef(null)
-
-    useEffect(() => {
-        listenToScrollEvent()
-        const ua = navigator.userAgent.toLowerCase()
-        if (ua.indexOf('safari') !== -1) {
-            if (ua.indexOf('chrome') > -1) {
-                setIsSafari(false) // Chrome
-            } else {
-                setIsSafari(true) // Safari
-            }
-        }
-    }, [])
-
-    const listenToScrollEvent = () => {
-        document.addEventListener('scroll', () => {
-            let scrolled: any
-            const yPos = window.scrollY
-            if (yPos < 150) scrolled = `20%`
-            if (yPos > 140) setDidScroll(true)
-            if (yPos > 150) {
-                scrolled = `${((yPos - 140) * 20) / 720 + 20}%`
-            }
-            if (yPos > 860) {
-                scrolled = `${((yPos - 860) * 20) / 650 + 40}%`
-            }
-            if (yPos > 1510) {
-                scrolled = `${((yPos - 1510) * 20) / 650 + 60}%`
-            }
-            if (yPos > 2160) {
-                scrolled = `${((yPos - 2160) * 20) / 2720 + 80}%`
-            }
-            if (yPos > 4880) scrolled = `100%`
-
-            return setScrollProgress(scrolled)
-        })
-    }
-
-    const handleScroll = (e: any) => {
-        const { id } = e.currentTarget
-        if (id === 'intro') return scrollToRef(heroRef)
-        if (id === 'about') return scrollToRef(aboutRef)
-        if (id === 'skills') return scrollToRef(skillsRef)
-        // if (id === 'exp') return scrollToRef(expRef)
-        // if (id === 'project') return scrollToRef(projectRef)
-
-        return scrollToRef(heroRef)
-    }
-
-    const scrollToRef = (ref: any) => window.scrollTo(0, ref.current.offsetTop)
-
-    const handleToast = (msg: any) => {
-        setToast((oldVal) => [...oldVal, msg])
-        setTimeout(() => {
-            setToast((oldState) => oldState.splice(1, 1))
-        }, 3000)
-    }
-
-    return (
-        <Theme>
-            <AppContainer>
-                <Navbar
-                    isSafari={isSafari}
-                    scrollProgress={scrollProgress}
-                    handleScroll={handleScroll}
-                    setDidScroll={setDidScroll}
-                />
-                <Sidebar handleToast={handleToast} />
-
-                <Main
-                    heroRef={heroRef}
-                    aboutRef={aboutRef}
-                    skillsRef={skillsRef}
-                    projectRef={projectRef}
-                    didScroll={didScroll}
-                />
-            </AppContainer>
-        </Theme>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p  className='text-3xl font-bold underline'>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+      <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 lg:w-1/3">
+        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
+          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          <a class="text-indigo-500 inline-flex items-center">Learn More
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>1.2K
+            </span>
+            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+              </svg>6
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-1/3">
+        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Ennui Snackwave Thundercats</h1>
+          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          <a class="text-indigo-500 inline-flex items-center">Learn More
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>1.2K
+            </span>
+            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+              </svg>6
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Selvage Poke Waistcoat Godard</h1>
+          <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          <a className="text-indigo-500 inline-flex items-center">Learn More
+            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+              </svg>6
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+  )
 }
 
 export default App
-function setToast(arg0: (oldVal: any) => any[]) {
-    throw new Error('Function not implemented.')
-}
