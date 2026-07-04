f74c90c8c56c3ccb696874a273aff8576686a50b
reverb6821 <giuseppegigliottidev@gmail.com>
2026-07-04 17:34:50 +0200

    chore: setted up project

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,65 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Tech from '@/components/tech';
-import Repository from '@/components/repository';
-import Timeline from '@/components/timeline';
-import { PROJECTS, HOME_TITLE, HOME_DESC, EXPERIENCES } from '@/data';
-import { FaVuejs } from "react-icons/fa";
-
-import { FaReact, FaCss3 } from 'react-icons/fa'
-import { RiTailwindCssFill, RiHtml5Line } from 'react-icons/ri'
-import { SiShadcnui, SiRedux, SiVitest, SiTestinglibrary } from 'react-icons/si'
-import { TbBrandTypescript } from 'react-icons/tb'
-import { LiaDocker } from 'react-icons/lia'
-import { GrMysql } from 'react-icons/gr'
-
-const TECHS = [
-    {
-      name: "HTML",
-      icon: <RiHtml5Line size={30} color='#ff5733' />
-  
-    },
-    {
-      name: "CSS",
-      icon: <FaCss3 size={30} color='#33b8ff'/>
-  
-    },
-    {
-      name: "Typescript",
-      icon: <TbBrandTypescript size={30} color='#1772ff' />
-    },
-    {
-      name: "Vue",
-      icon: <FaVuejs size={30} color='#27bb61' />
-  
-    },
-    {
-      name: "ReactJS",
-      icon: <FaReact size={30} color='#3383ff'/>
-    },
-    {
-      name: "Redux",
-      icon: <SiRedux size={30} color='#cf02ec'/>
-    },
-    {
-      name: "RTL",
-      icon: <SiTestinglibrary size={30} color='#e03446' />
-    },
-    {
-      name: "Vitest",
-      icon: <SiVitest size={30} color='#dfdc11'/>
-    },
-    {
-      name: "Tailwind CSS",
-      icon: <RiTailwindCssFill size={30} color='#0baac3'/>
-    },
-    {
-      name: "Shadcn-ui",
-      icon: <SiShadcnui size={30}  className='color-slate-900 dark:color-slate-50'/>
-    },
-    {
-      name: "Docker",
-      icon: <LiaDocker size={30} color='#3c9ac6'/>
-    },
-    {
-      name: "Mysql",
-      icon: <GrMysql size={30} color='#e57d35' />
-    }
-  ]
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <React.Fragment>
-      <div className="full-container flex">
-        <div className="space-y-4">
-          <h1 className="text-4xl font-semibold">
-            <Link href="/">
-              {HOME_TITLE}
-            </Link>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
           </h1>
-          <p className="mt-5 font-normal text-color-secondary leading-relaxed">
-            {HOME_DESC}
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
           </p>
         </div>
-      </div>
-
-       <div className="full-container mt-10">
-            <Tech title='Tech' techs={TECHS}/>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
         </div>
-
-        <div className="full-container mt-20">
-            <Repository title='My GitHub Repositories' projects={PROJECTS}/>
-        </div>
-
-        <div className="full-container mt-10">
-            <Timeline title='My Experience' experiences={EXPERIENCES}/>
-        </div>
-    </React.Fragment>
+      </main>
+    </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'build',
 };
 
 export default nextConfig;
